98f82f80399cd5bba3e465c4d39f2e51cb5578c5
cqw-acq <133117051+cqw-acq@users.noreply.github.com>
2024-09-23 17:02:18 +0800

    Update fin_pra_4_9.java

--- src/fin_pra_4_9.java
+++ src/fin_pra_4_9.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class fin_pra_4_9 {
     public static void main(String[] args) throws IOException {
         int number = 1;
-        System.in.read ();
+        number=System.in.read();
         if (number % 2 == 0){
             System.out.println("even");
         }else {
