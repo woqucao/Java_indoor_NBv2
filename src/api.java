465b8cd5306e8bc216115a0ff1313ebe395c271c
cqw-acq <chuqijerry@outlook.com>
2024-10-21 14:21:10 +0800

    creat the algorithm part of the 'totoapi'(i call it totoapi for my api practice)

--- src/api.java
+++ src/api.java
@@ -1,2 +1,26 @@
 public class api {
+    public static String getGrade(int score) {
+        if (score >= 90) {
+            return "优秀";
+        } else if (score >= 80) {
+            return "良";
+        } else if (score >= 60) {
+            return "及格";
+        } else {
+            return "不及格";
+        }
+    }
+    public static int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
 }
